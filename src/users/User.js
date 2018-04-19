f60f4f175d541067f809bf74f1bdd5ece4bb26af
Ruthbai <minjoobai@gmail.com>
2018-04-19 13:36:01 +1200

    studying on State and props

--- src/users/User.js
+++ src/users/User.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
 const User = (props) => {
-    let age = props.age ? props.age : 'NA';
-    if(props.children){
-    return (<div>Name: {props.children} | Age: {age}</div>)
-    }else {
-        return <div>Invalid Entry</div>
-    }
+    return (<div>Name: {props.children} | Age: {props.age}</div>)
 }
 
 export default User;
--- src/users/Users.js
+++ src/users/Users.js
@@ -2,14 +2,40 @@ import React, { Component } from 'react';
 import User from './User';
 
 class Users extends Component {
+    state = {
+        users: [
+        {name:"Ruth", age:24},
+        {name:"Ruthy", age:40},
+        {name:"Ruthliana", age:1},
+        ],
+        title:"Users List"
+    }
+
+
+    makeMeYounger = () => {
+        const newState = this.state.users.map((user)=>{
+            const tempUser = user;
+            tempUser.age -=5;
+            return tempUser;
+        });
+        this.setState({
+            newState
+        });
+    }
+
+
     render(){
         return (
         <div>
-            <h1>{this.props.title}</h1>
-            <User>Ruth</User>
-            <User></User>
-            <User age="50">Ruthlina</User>
-            <User age="1">Rumio</User>
+            <button onClick ={this.makeMeYounger}>Make us 5 years younger</button>
+            <br/>
+            <h1>{this.state.title}</h1>
+            {
+             this.state.users.map((user)=>{
+                 return <User age={user.age}>{user.name}</User>
+             })
+            }
+            
         </div>
         )
     }
